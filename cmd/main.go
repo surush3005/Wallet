0ff37bd8814988a9500f5da2ba171f3eae18e45d
surush3005 <surush3005@gmail.com>
2026-02-21 15:56:31 +0500

    add changes

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/surush3005/Wallet/pkg/wallet"
+)
+
+func main() {
+
+	svc := &wallet.Service{}
+	/*
+		wallet.RegisterAccount(svc, "+992918176779")
+	*/
+
+	//svc.RegisterAccount("+992918176779")
+
+	account, err := svc.RegisterAccount("+992918176779")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(account)
+		return
+	}
+
+}
--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,24 +1,54 @@
 package wallet
 
-import "github.com/surush3005/Wallet/pkg/types"
+import (
+	"errors"
+
+	"github.com/surush3005/Wallet/pkg/types"
+)
 
 type Service struct {
-	accounts []types.Amountcont
-	payments []types.Payment
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
 }
 
-func RegisterAccount(service *Service, phone types.Phone) {
-	for _, accounts := range service.accounts {
-		if accounts.Phone == phone {
-			return
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, errors.New("Phone already registered")
 		}
 	}
 
-	service.nextAccountId++
+	s.nextAccountID++
 
-	service.accounts = append(service.accounts, types.Account{
-		ID:      service.nextAccountID,
+	account := &types.Account{
+		ID:      s.nextAccountID,
 		Phone:   phone,
 		Balance: 0,
-	})
+	}
+
+	s.accounts = append(s.accounts, account)
+
+	return account, nil
+}
+
+func (s *Service) Deposit(AccountId int64, amount types.Money) {
+	if amount <= 0 {
+
+		return
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == AccountId {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return
+	}
+
+	account.Balance += amount
 }
